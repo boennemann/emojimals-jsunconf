ea31d47e6ab521347da5feaed61cd4a148214e6e
Stephan Bönnemann <stephan@boennemann.me>
2015-04-25 22:26:00 +0200

    test(animal-to-emoji): bugs don't hiss

--- tests/sound-to-emoji.js
+++ tests/sound-to-emoji.js
@@ -3,8 +3,9 @@ var test = require('tape')
 var soundToEmojis = require('../')
 
 test(function (t) {
-  t.plan(2)
+  t.plan(3)
 
   t.ok(/🐈/.test(soundToEmojis('hiss')), 'cats hiss')
+  t.ok(!/🐛/.test(soundToEmojis('hiss')), 'bugs don\'t hiss')
   t.is(soundToEmojis('yo'), null, 'yo is not an animal sound')
 })
