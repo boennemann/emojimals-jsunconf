fdaa2cc7e34197aea08724262a35429d2f9318bb
Stephan Bönnemann <stephan@boennemann.me>
2015-04-25 17:00:01 +0200

    feat(sound-to-emoji): return funny message if no animal was found

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var soundToAnimals = require('./sound-to-animals.json')
 var sounds = Object.keys(soundToAnimals)
 
 module.exports = function (input) {
-  if (!~sounds.indexOf(input)) return null
+  if (!~sounds.indexOf(input)) return 'NOPE 🐙  NOPE 🐙  NOPE 🐙'
 
   return (soundToAnimals[input].map(function (animal) {
     return animalToEmoji[animal]
