1f6478fec7af0e58813d425eec735006b71d2a6c
Stephan Bönnemann <stephan@boennemann.me>
2015-04-25 16:40:17 +0200

    feat(sound-to-animals): basic functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+var animalToEmoji = require('./animal-to-emoji.json')
+var soundToAnimals = require('./sound-to-animals.json')
+
+var sounds = Object.keys(soundToAnimals)
+
+module.exports = function (input) {
+  if (!~sounds.indexOf(input)) return null
+
+  return (soundToAnimals[input].map(function (animal) {
+    return animalToEmoji[animal]
+  }).join(' '))
+}
